1f7c5ce8b46d6609457370b416a690fef0395b27
ptavarez16 <ptavarez16@yahoo.com>
2018-06-15 12:59:47 -0400

    Initial Commit

---
+++ config/environment.js
@@ -0,0 +1,56 @@
+/* eslint-env node */
+
+module.exports = function (environment) {
+  'use strict'
+  const ENV = {
+    modulePrefix: 'contact-me',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    apiHost: 'http://localhost:4741/',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  }
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true
+    // ENV.APP.LOG_ACTIVE_GENERATION = true
+    // ENV.APP.LOG_TRANSITIONS = true
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true
+    // ENV.APP.LOG_VIEW_LOOKUPS = true
+    ENV.apiHost = 'http://localhost:4741'
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.rootUrl = '/'
+    ENV.locationType = 'none'
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
+
+    ENV.APP.rootElement = '#ember-testing'
+  }
+
+  if (environment === 'production') {
+    ENV.rootURL = '/contact-me'
+    ENV.locationType = 'hash'
+    ENV.apiHost = 'https://contact-me-rails.herokuapp.com'
+  }
+
+  return ENV
+}
