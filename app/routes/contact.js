dfe37e8e08e41e1765d656bddbc9f9cef364907e
ptavarez16 <ptavarez16@yahoo.com>
2018-06-15 23:29:55 -0400

    Add updateContact action

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -3,5 +3,11 @@ import Route from '@ember/routing/route'
 export default Route.extend({
   model (params) {
     return this.get('store').findRecord('contact', params.contact_id)
+  },
+
+  actions: {
+    updateContact (contact) {
+      contact.save()
+    }
   }
 })
