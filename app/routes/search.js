0a24de73e30f9aa4b2c0687d68d42836dfb7b652
ptavarez16 <ptavarez16@yahoo.com>
2018-06-17 13:41:17 -0400

    Add search - Generate route search - Add search bar to contacts view

---
+++ app/routes/search.js
@@ -0,0 +1,21 @@
+import Route from '@ember/routing/route'
+
+export default Route.extend({
+  model: function (params) {
+    const searchQuery = params.searchQuery
+    console.log('You be searching for', searchQuery)
+    return this.get('store').query('contact', { search: searchQuery })
+      .then((results) => {
+        if (results.get('lastObject') === undefined) {
+          results = false
+          console.log('Contact(s) not found')
+        } else {
+          console.log('Contact(s) found!', results)
+        }
+        return results
+      })
+  },
+
+  actions: {
+  }
+})
