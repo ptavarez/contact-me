d56b9e52fe72cec7a36afef128fb456a10def950
ptavarez16 <ptavarez16@yahoo.com>
2018-06-17 13:33:58 -0400

    Add sign out to change password page - Add sign out button - Call action 'sign out' to sign user out and redirect to landing page

--- app/routes/change-password.js
+++ app/routes/change-password.js
@@ -31,6 +31,19 @@ export default Route.extend({
         this.get('flashMessages')
         .danger('Your new passwords must match.')
       }
+    },
+
+    signOut () {
+      this.get('auth').signOut()
+        .then(() => this.get('store').unloadAll())
+        .then(() => this.transitionTo('landing'))
+        .then(() => {
+          this.get('flashMessages').warning('You have been signed out.')
+        })
+        .catch(() => {
+          this.get('flashMessages')
+          .danger('There was a problem. Are you sure you\'re signed-in?')
+        })
     }
   }
 })
